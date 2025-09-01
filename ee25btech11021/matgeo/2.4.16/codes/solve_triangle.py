7f45feb873a128a9dbca4c54656aa2b9a8f128d7
dhanushsagarEE11021 <dhanushsagar2025@gmail.com>
2025-09-01 16:50:13 +0530

    Add files via upload

--- ee25btech11021/matgeo/2.4.16/codes/solve_triangle.py
+++ ee25btech11021/matgeo/2.4.16/codes/solve_triangle.py
@@ -1,33 +1,43 @@
 
+
 import ctypes
 import numpy as np
 
-# Load the shared object
+# Load shared object
 lib = ctypes.CDLL("./gen_points.so")
 
-# Call the C function to generate points.dat
-lib.generate_points(b"points.dat")
+# Call C functions
+lib.generate_points_isosceles(b"iso_points.dat")
+lib.generate_points_right(b"right_points.dat")
 
-# Load points from file
-points = np.loadtxt("points.dat")
-A, B, C = points
+# Load points
+iso_points = np.loadtxt("iso_points.dat")
+right_points = np.loadtxt("right_points.dat")
 
-# Function to compute squared distance
+# Distance squared function
 def dist2(P, Q):
     return np.sum((P - Q) ** 2)
 
-# Squared lengths
+# --------- Part (a) Isosceles Triangle -----------
+A, B, C = iso_points
 AB2 = dist2(A, B)
 BC2 = dist2(B, C)
 CA2 = dist2(C, A)
 
-print("Squared lengths:")
-print("AB^2 =", AB2, " BC^2 =", BC2, " CA^2 =", CA2)
-
-# Check isosceles (two sides equal)
+print("Isosceles Triangle Check:")
+print("AB^2 =", AB2, "BC^2 =", BC2, "CA^2 =", CA2)
 isosceles = (AB2 == BC2) or (BC2 == CA2) or (CA2 == AB2)
-print("Isosceles Triangle:", isosceles)
+print("Isosceles:", isosceles)
+print()
 
-# Check right angle (Pythagoras theorem)
-right_angle = (AB2 + BC2 == CA2) or (BC2 + CA2 == AB2) or (CA2 + AB2 == BC2)
-print("Right Angled Triangle:", right_angle)
+# --------- Part (b) Right Angled Triangle ---------
+P, Q, R = right_points
+PQ2 = dist2(P, Q)
+QR2 = dist2(Q, R)
+RP2 = dist2(R, P)
+
+print("Right Angled Triangle Check:")
+print("PQ^2 =", PQ2, "QR^2 =", QR2, "RP^2 =", RP2)
+right_angle = (PQ2 + QR2 == RP2) or (QR2 + RP2 == PQ2) or (RP2 + PQ2 == QR2)
+print("Right Angled:", right_angle)
+
