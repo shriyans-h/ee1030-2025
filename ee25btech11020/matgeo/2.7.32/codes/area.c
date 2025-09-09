43564394ce6434830b82db34ae5659e62ed8d42c
gajaredarsh <darshgajare@gmail.com>
2025-09-09 16:19:47 +0530

    ee25btech11020

--- ee25btech11020/matgeo/2.7.32/codes/area.c
+++ ee25btech11020/matgeo/2.7.32/codes/area.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-double area(double *a,double *b, double *c,int n)
-{
-	double area;
+#include <math.h>
+
+double triangle_area(double p1[2], double p2[2], double p3[2]) {
+    double area = 0.5 * fabs(
+        p1[0]*(p2[1] - p3[1]) +
+        p2[0]*(p3[1] - p1[1]) +
+        p3[0]*(p1[1] - p2[1])
+    );
+    return area;
 }
