8bd08b6902a0af8dac7721e50d28d0e04c395479
SARVESSHTAMGADE <sarveshtamgade65@gmail.com>
2025-09-10 21:10:13 +0530

    commit

--- AI25BTECH11030/Matgeo/2.10.20/CODES/vector.py
+++ AI25BTECH11030/Matgeo/2.10.20/CODES/vector.py
@@ -1,35 +1,33 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-# Define vectors
-u = np.array([1, 2, 3])
-v = np.array([4, 5, 6])
-w = np.array([7, 8, 9])
+# Vectors u, v, w in 2D (using first two components)
+u = np.array([2, 3])
+v = np.array([4, 1])
+w = np.array([0, 5])
 
-# Cross product v × w
-cross_vw = np.cross(v, w)
+# Origin point
+origin = np.array([0, 0])
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# Plotting the vectors
+plt.figure(figsize=(7, 7))
+plt.quiver(*origin, *u, angles='xy', scale_units='xy', scale=1, color='red', label='u = [2, 3]')
+plt.quiver(*origin, *v, angles='xy', scale_units='xy', scale=1, color='green', label='v = [4, 1]')
+plt.quiver(*origin, *w, angles='xy', scale_units='xy', scale=1, color='blue', label='w = [0, 5]')
 
-# Plot vectors
-ax.quiver(0, 0, 0, u[0], u[1], u[2], color='r', label='u')
-ax.quiver(0, 0, 0, v[0], v[1], v[2], color='g', label='v')
-ax.quiver(0, 0, 0, w[0], w[1], w[2], color='b', label='w')
-ax.quiver(0, 0, 0, cross_vw[0], cross_vw[1], cross_vw[2], color='m', label='v×w')
+# Setting the limits
+plt.xlim(-1, 5)
+plt.ylim(-1, 6)
 
-ax.set_xlim([0, 10])
-ax.set_ylim([0, 10])
-ax.set_zlim([0, 10])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.legend()
-plt.title('Vector visualization')
+# Adding labels and title
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('2D vectors: u, v, w')
+plt.grid()
+plt.legend()
+plt.gca().set_aspect('equal')
 
-# Optional: save the figure as an image
-plt.savefig('vector_plot.png')
-
-plt.show()
+# Save the figure as a PNG file
+plt.savefig('2D_vectors.png')
+plt.close()
 
