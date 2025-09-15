c22874514fc218fe9f843b0b95367b89c3f97d01
Venkatasaiee12 <dvenkatasai2025@gmail.com>
2025-09-16 04:11:07 +0530

    commit:

---
+++ ee25btech11023/Assignments/matgeo/5.13.49/codes/code.c
@@ -0,0 +1,13 @@
+void get_system_coeffs(double* out_coeffs) {
+     
+    out_coeffs[0] = 1.0;
+    out_coeffs[1] = -1.0;
+   
+    out_coeffs[2] = -1.0;
+    out_coeffs[3] = -1.0;
+    
+
+    out_coeffs[4] = 1.0;
+    out_coeffs[5] = 1.0;
+    out_coeffs[6] = 1.0;
+}
