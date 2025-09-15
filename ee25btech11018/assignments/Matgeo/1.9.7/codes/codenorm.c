8f4d302c4ea07ee9b811166f42fab54ee1cf709b
sreetejEE <sreetej2803@gmail.com>
2025-09-15 15:13:25 +0530

    Matgeo4

--- ee25btech11018/assignments/Matgeo/1.9.7/codes/codenorm.c
+++ ee25btech11018/assignments/Matgeo/1.9.7/codes/codenorm.c
@@ -6,11 +6,4 @@ double norm(int x, int y) {
     return sqrt(x*x + y*y);
 }
 
-int main() {
-    int x = -6, y = 8;
-    printf("Given point: (%d, %d)\n", x, y);
-    double distance = norm(x,y);
-    printf("Therefore, the distance of the point (%d, %d) from the origin is %.2f units.\n", x, y, distance);
-    return 0;
-}
 
