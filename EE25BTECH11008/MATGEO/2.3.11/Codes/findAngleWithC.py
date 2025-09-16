bc27a5969ce3c765db5e489e66282bce1873a390
Anirudh-EE25BTECH11008 <ee25btech11008@iith.ac.in>
2025-09-17 03:25:05 +0530

    Matgeo_3 Take_4

--- EE25BTECH11008/MATGEO/2.3.11/Codes/findAngleWithC.py
+++ EE25BTECH11008/MATGEO/2.3.11/Codes/findAngleWithC.py
@@ -1,42 +1,20 @@
 import ctypes
 import numpy
 
-# Load the shared library
 lib = ctypes.CDLL("./computations.so")
 
-# Define function signatures
-lib.gram_matrix.argtypes = [
-    ctypes.POINTER(ctypes.c_double),
-    ctypes.POINTER(ctypes.c_double),
-    ctypes.c_int,
-    ctypes.POINTER((ctypes.c_double * 2) * 2)
+lib.find_angle.argtypes = [
+    ctypes.POINTER((ctypes.c_int * 3)),
+    ctypes.POINTER((ctypes.c_int * 3))
 ]
+lib.find_angle.restype = ctypes.c_double
 
-lib.normalize_gram.argtypes = [
-    ctypes.POINTER((ctypes.c_double * 2) * 2),
-    ctypes.POINTER((ctypes.c_double * 2) * 2)
-]
-
-lib.angle_from_normalized.argtypes = [
-    ctypes.POINTER((ctypes.c_double * 2) * 2)
-]
-lib.angle_from_normalized.restype = ctypes.c_double
-
-# Python wrapper
 def compute_angle(u, v):
-    n = len(u)
-    u_arr = (ctypes.c_double * n)(*u)
-    v_arr = (ctypes.c_double * n)(*v)
-
-    G = ((ctypes.c_double * 2) * 2)()
-    G_norm = ((ctypes.c_double * 2) * 2)()
-
-    lib.gram_matrix(u_arr, v_arr, n, G)
-    lib.normalize_gram(G, G_norm)
-    theta = lib.angle_from_normalized(G_norm)
+    u_arr = (ctypes.c_int * 3)(*u)
+    v_arr = (ctypes.c_int * 3)(*v)
+    theta = lib.find_angle(u_arr, v_arr)
     return theta
 
-# Main
 u = [1, -2, -2]
 v = [3, -6, 2]
 theta = compute_angle(u, v)
