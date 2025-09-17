9fde9f7a784ecfff917390381d9b6eb6a94c907e
Vivaan Parashar <parashar.xtra@gmail.com>
2025-09-17 20:19:42 +0530

    2.10.29 again

--- ai25btech11040/matgeo/2.10.29/code/code.c
+++ ai25btech11040/matgeo/2.10.29/code/code.c
@@ -1,16 +1,9 @@
 #include <math.h>
-double cross_product_x(double a_x, double a_y, double a_z, double b_x, double b_y, double b_z) {
-    return (a_y * b_z - a_z * b_y);
-}
-
-double cross_product_y(double a_x, double a_y, double a_z, double b_x, double b_y, double b_z) {
-    return (a_z * b_x - a_x * b_z);
-}
-
-double cross_product_z(double a_x, double a_y, double a_z, double b_x, double b_y, double b_z) {
-    return (a_x * b_y - a_y * b_x);
-}
-
-double dot_product(double a_x, double a_y, double a_z, double b_x, double b_y, double b_z) {
-    return a_x * b_x + a_y * b_y + a_z * b_z;
+double volume(double a_x, double a_y, double a_z, double b_x, double b_y, double b_z, double c_x, double c_y, double c_z) {
+    int g[3][3] = {
+        {a_x*a_x + a_y*a_y + a_z*a_z, a_x*b_x + a_y*b_y + a_z*b_z, a_x*c_x + a_y*c_y + a_z*c_z},
+        {b_x*a_x + b_y*a_y + b_z*a_z, b_x*b_x + b_y*b_y + b_z*b_z, b_x*c_x + b_y*c_y + b_z*c_z},
+        {c_x*a_x + c_y*a_y + c_z*a_z, c_x*b_x + c_y*b_y + c_z*b_z, c_x*c_x + c_y*c_y + c_z*c_z}
+    };
+    return sqrt(fabs(g[0][0]*(g[1][1]*g[2][2] - g[1][2]*g[2][1]) - g[0][1]*(g[1][0]*g[2][2] - g[1][2]*g[2][0]) + g[0][2]*(g[1][0]*g[2][1] - g[1][1]*g[2][0])));
 }
--- ai25btech11040/matgeo/2.10.29/code/code_with_c.py
+++ ai25btech11040/matgeo/2.10.29/code/code_with_c.py
@@ -6,27 +6,10 @@ b = np.array([1, 1, -1])
 c = np.array([3, 0, -1])
 
 lib = CDLL("./code.so")
-lib.cross_product_x.argtypes = [c_double] * 6
-lib.cross_product_y.argtypes = [c_double] * 6
-lib.cross_product_z.argtypes = [c_double] * 6
-lib.dot_product.argtypes = [c_double] * 6
+lib.volume.argtypes = [c_double] * 9
 
+lib.volume.restype = c_double
 
-lib.cross_product_x.restype = c_double
-lib.cross_product_y.restype = c_double
-lib.cross_product_z.restype = c_double
-lib.dot_product.restype = c_double
-
-b_cross_c = np.array(
-    [
-        lib.cross_product_x(*b, *c),
-        lib.cross_product_y(*b, *c),
-        lib.cross_product_z(*b, *c),
-    ]
-)
-
-volume = abs(
-    lib.dot_product(*a, *b_cross_c)
-)
+volume = lib.volume(a[0], a[1], a[2], b[0], b[1], b[2], c[0], c[1], c[2])
 
 print(f"Volume of the parallelopiped is {volume}")
