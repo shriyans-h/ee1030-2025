8b88eb02d3fdb1a9d5afe77226ad1d1b1d19a5fe
Vivaan Parashar <parashar.xtra@gmail.com>
2025-09-24 02:22:45 +0530

    finish q4.4.36

--- ai25btech11040/matgeo/4.4.36/code/code.py
+++ ai25btech11040/matgeo/4.4.36/code/code.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 fig.suptitle("Q 4.4.36")
 
+plt.rcParams['text.usetex'] = True
+
 # x/a + y/b = 1
 # => y = b(1 - x/a)
 
@@ -13,8 +15,7 @@ b = 6
 x_in = np.array([a, 0])
 y_in = np.array([0, b])
 
-ax.axline(x_in, y_in, color="blue", label="Arbitrary line")
-
+ax.axline(x_in, y_in, color="blue", label=f"Arbitrary line $\\frac{{x}}{{{a}}} + \\frac{{y}}{{{b}}} = 1$")
 
 ax.plot(*x_in, "ro")
 ax.plot(*y_in, "ro")
@@ -33,29 +34,31 @@ ax.annotate("O", (0, 0), textcoords="offset points", xytext=(-10, -15), ha="cent
 
 ax.annotate(
     "a",
-    (a / 2, 0),
+    (a/2, -0.5),
     textcoords="offset points",
-    xytext=(-10, 0),
+    xytext=(0,-15),
     ha="center",
+    va='bottom',
     arrowprops=dict(
-        arrowstyle=f"-[, widthB={a}, lengthB=0.5",  # Bracket style with width and length
-        lw=1.5,  # Line width of the bracket
-        color="red",  # Color of the bracket
+        arrowstyle=f"-[, widthB={0.5*a}, lengthB=0.2",
+        lw=1.5,
+        color="red",
     ),
     fontsize=12,
     color="blue",
 )
+
 ax.annotate(
     "b",
-    (0, b / 2),
+    (-0.5, b/2),
     textcoords="offset points",
-    xytext=(0,b),
+    xytext=(-10,-5.3),
     ha="center",
     va='bottom',
     arrowprops=dict(
-        arrowstyle=f"-[, widthB={b}, lengthB=0.5",  # Bracket style with width and length
-        lw=1.5,  # Line width of the bracket
-        color="red",  # Color of the bracket
+        arrowstyle=f"-[, widthB={0.6*b}, lengthB=0.2",
+        lw=1.5,
+        color="red",
     ),
     fontsize=12,
     color="blue",
@@ -74,4 +77,4 @@ ax.spines["left"].set_position("zero")
 ax.spines["bottom"].set_position("zero")
 
 # plt.show()
-fig.savefig("../figs/plot.png")
+fig.savefig("../figs/plot.png", dpi=300)
