76ad6b31d558ead481a030aa5abc12b55f637e4f
KartikLahoti-iith <your-kartiklahoti11@gmail.com>
2025-10-03 23:09:13 +0530

    ee25btech11032

---
+++ ee25btech11032/matgeo/12.670/codes/func.c
@@ -0,0 +1,20 @@
+#include <math.h>
+void solve(double a , double b , double c , double *real , double *img)
+{
+
+	double temp = pow(b,2)- 4*a*c;
+	if( temp >= 0 )
+	{
+		for(int i = 0 ; i < 2 ; i++)
+		{
+			real[i] = -b+pow((-1),i)*sqrt(temp);
+			real[i] /=2 ;
+		}
+			
+	}
+	else
+	{
+
+	}
+
+}
