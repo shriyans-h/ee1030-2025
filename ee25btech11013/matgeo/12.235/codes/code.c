678447524d49adf5bd289a2e82586c8863dcc783
Bhargav-Krishnamurthy <bhargavkrish18@gmail.com>
2025-10-05 16:24:36 +0000

    updated

--- ee25btech11013/matgeo/12.235/codes/code.c
+++ ee25btech11013/matgeo/12.235/codes/code.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
-int determinant(int mat[3][3]) {
-    int det;
-    det = mat[0][0]*(mat[1][1]*mat[2][2] - mat[1][2]*mat[2][1])
-        - mat[0][1]*(mat[1][0]*mat[2][2] - mat[1][2]*mat[2][0])
-        + mat[0][2]*(mat[1][0]*mat[2][1] - mat[1][1]*mat[2][0]);
-    return det;
+void solve_planes(double *A, double *b, double *x) {
+    double detA, det1, det2, det3;
+
+    detA = A[0]*(A[4]*A[8] - A[5]*A[7]) -
+           A[1]*(A[3]*A[8] - A[5]*A[6]) +
+           A[2]*(A[3]*A[7] - A[4]*A[6]);
+
+    if(detA == 0) {
+        x[0] = x[1] = x[2] = 0;
+        return;
+    }
+
+    det1 = b[0]*(A[4]*A[8] - A[5]*A[7]) -
+           A[1]*(b[1]*A[8] - A[5]*b[2]) +
+           A[2]*(b[1]*A[7] - A[4]*b[2]);
+
+    det2 = A[0]*(b[1]*A[8] - A[5]*b[2]) -
+           b[0]*(A[3]*A[8] - A[5]*A[6]) +
+           A[2]*(A[3]*b[2] - b[1]*A[6]);
+
+    det3 = A[0]*(A[4]*b[2] - b[1]*A[7]) -
+           A[1]*(A[3]*b[2] - b[1]*A[6]) +
+           b[0]*(A[3]*A[7] - A[4]*A[6]);
+
+    x[0] = det1 / detA;
+    x[1] = det2 / detA;
+    x[2] = det3 / detA;
 }
